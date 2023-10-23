a6bbeead0bf650e3d52bc68d52f284cc23beef6a
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-10-23 13:41:35 +0700

    fixing run-server.sh

--- run_server.sh
+++ run_server.sh
@@ -5,12 +5,12 @@ function check_config() {
     param="$1"
     if [[ -n "$2" ]]
     then
-#        echo "not Empty";
+        echo "not Empty";
         value="$2"
         DB_ARGS+=("--${param}")
         DB_ARGS+=("${value}")
-#    else
-#        echo "empty"
+    else
+        echo "empty"
     fi
 }
 
@@ -21,12 +21,12 @@ check_config "db_password" "$DB_PASSWORD"
 check_config "database" "$DB_NAME"
 
 echo "PARAMS: " "${DB_ARGS[@]}"
-./wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+./wait-for-psql.py ${DB_ARGS[@]} --timeout=30 &&
 if [ ! -f /etc/init_odoo.lock ]; then
     echo "Odoo server need init!" &&
     touch /etc/odoo-config/init_odoo.lock &&
     psql postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME -c "select 'drop table if exists \"' || tablename || '\" cascade;'  from pg_tables where schemaname = 'public';"&&
     python3 ./odoo-bin -c /server/setup/odoo-server.conf "${DB_ARGS[@]}" -i base --stop-after-init &&
     echo "Odoo server init successful"
-fi
+fi &&
 python3 ./odoo-bin -c /server/setup/odoo-server.conf "${DB_ARGS[@]}"
