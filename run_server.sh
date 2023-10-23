784d766f30903abab5ef3ea38cd585f60b1fe7e4
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-10-23 16:51:21 +0700

    update run_server.sh: trying to debug

--- run_server.sh
+++ run_server.sh
@@ -21,12 +21,13 @@ check_config "db_password" "$DB_PASSWORD"
 check_config "database" "$DB_NAME"
 
 echo "PARAMS: " "${DB_ARGS[@]}"
-./wait-for-psql.py ${DB_ARGS[@]} --timeout=30 &&
+./wait-for-psql.py ${DB_ARGS[@]} --timeout=30 && \
 if [ ! -f /etc/init_odoo.lock ]; then
-    echo "Odoo server need init!" &&
-    touch /etc/odoo-config/init_odoo.lock &&
-    psql postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$DB_NAME' AND pid <> pg_backend_pid(); select 'drop table if exists \"' || tablename || '\" cascade;'  from pg_tables where schemaname = 'public';"&&
+    echo "Odoo server need init!" && \
+    touch /etc/odoo-config/init_odoo.lock && \
+    psql postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$DB_NAME' AND pid <> pg_backend_pid(); select 'drop table if exists \"' || tablename || '\" cascade;'  from pg_tables where schemaname = 'public';"&& \
+    echo "Initializing..." && \
     python3 ./odoo-bin -c /server/setup/odoo-server.conf --db_host satao.db.elephantsql.com --db_user gyufpkyg --db_password ysAWVA_u74TwGb0Obp-7N4y4tUbERCIB --database gyufpkyg -i base --stop-after-init &&
     echo "Odoo server init successful"
-fi &&
+fi && \
 python3 ./odoo-bin -c /server/setup/odoo-server.conf "${DB_ARGS[@]}"
